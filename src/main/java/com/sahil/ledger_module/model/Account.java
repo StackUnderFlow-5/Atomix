63696ac865c81fca43e46601cca7f4e12e2448b9
Sahil Behera <sahilbehera0999@gmail.com>
2026-02-17 20:09:09 +0530

    feat: project setup, H2 config, and account entity

---
+++ src/main/java/com/sahil/ledger_module/model/Account.java
@@ -0,0 +1,22 @@
+package com.sahil.ledger_module.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String accountName;
+    private Double balance;
+}
---
+++ src/test/java/com/sahil/ledger_module/LedgerModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.ledger_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
